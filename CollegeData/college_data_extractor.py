27ffd2cc9bb1d2f965baad329cb6b64782a2e8c3
andrew-lockwood <andrewlockwood579@gmail.com>
2017-02-27 15:21:04 -0500

    data!

--- CollegeData/college_data_extractor.py
+++ CollegeData/college_data_extractor.py
@@ -4,69 +4,99 @@ import csv
 import os 
 import re
 
-html_dir = 'HTML'
+def write_to_csv(csv_file, html_dir, state):
+	with open(csv_file, 'w', newline='') as data: 
+		fieldnames = ['University', 'State', 'City', 'Enrollment (Undergrad)',
+						'Male Enrollment', 'Female Enrollment',
+						'Native Hawaiian/ Pacific Islander', 
+						'American Indian/Alaskan Native', 
+						'Multi-race (not Hispanic/Latino)', 'Asian', 
+						'White', 'Black/African-American', 'International', 
+						'Hispanic/Latino', 'Unknown']
+
+		writer = csv.DictWriter(data, fieldnames=fieldnames)
+		writer.writeheader()
+
+		for f in os.listdir(os.path.join(html_dir, state)):
+			row = dict()
+
+			with open(os.path.join(html_dir, state, f)) as html: 
+				soup = BeautifulSoup(html, 'lxml')
 
 
-for f in os.listdir(html_dir):
-	# Skip the readme file
-	if f == "README.md":
-		continue
+				title = soup.find("div", class_="cp_left")	
+				uni_name = title.find("h1").get_text()
+				uni_city, uni_state = title.find("p").get_text().strip().split(",")
 
-	path = os.path.join(html_dir, f)
-	with open(path) as html: 
-		soup = BeautifulSoup(html, 'lxml')
+				row['University'] = uni_name
+				row['State'] = uni_state.strip()
+				row['City'] = uni_city
+ 
+				undergrad_data = soup.find('th', text="Undergraduate Students")
 
-		# College Information
-		title = soup.find("div", class_="cp_left")	
-		uni_name = title.find("h1").get_text()
-		uni_city, uni_state = title.find("p").get_text().strip().split(",")
+				uni_size = undergrad_data.next_sibling.text
 
-		undergrad_data = soup.find('th', text="Undergraduate Students")
+				if uni_size == 'Not reported':
+					continue
 
-		uni_size = undergrad_data.next_sibling.text
+				row['Enrollment (Undergrad)'] = int(re.sub(",", "", uni_size))
 
-		# Gender information
-		women = undergrad_data.parent.next_sibling.next_sibling
-		enrolled_women = women.find('td').text.split()[0]
+				# Gender information
+				women = undergrad_data.parent.next_sibling.next_sibling
+				enrolled_women = women.find('td').text.split()[0]
 
-		men = women.next_sibling.next_sibling
-		enrolled_men = men.find('td').text.split()[0]
+				men = women.next_sibling.next_sibling
+				enrolled_men = men.find('td').text.split()[0]
 
-		# Economic information
-		#money = soup.find('th', text="Cost of Attendance")
-		#print("Cost: " + money.next_sibling.text)
+				if enrolled_women == 'Not':
+					continue
+				if enrolled_men == 'Not':
+					continue
 
-		# Greek life data -- would need to be incorporated  
-		#frat = soup.find('th', text="Fraternities")
-		#print("Frat: " + frat.next_sibling.text)
-		#soro = soup.find('th', text="Sororities")
-		#print("Soro: " + soro.next_sibling.text)
+				row['Male Enrollment'] = int(re.sub(",", "", enrolled_men))
+				row['Female Enrollment'] = int(re.sub(",", "", enrolled_women))
 
-		ethnicity = soup.find('th', text="Ethnicity of Students from U.S.")
 
-		ethnicity_dict = {}
+				# Economic information
+				#money = soup.find('th', text="Cost of Attendance")
+				#print("Cost: " + money.next_sibling.text)
 
-		for x in ethnicity.next_sibling.children:
-			if x.string != None:
-				try:
-					perc, nationality = x.string.split("%")
-					ethnicity_dict[nationality.strip()] = perc
-				except ValueError: 
-					print("ERROR")
-					print(x.string)
-		# International student data -- internat[2] is the number of countries
-		internat = soup.find('th', text="International Students").next_sibling.text.split()
-		p_internat = internat[0]
+				# Greek life data -- would need to be incorporated  
+				#frat = soup.find('th', text="Fraternities")
+				#print("Frat: " + frat.next_sibling.text)
+				#soro = soup.find('th', text="Sororities")
+				#print("Soro: " + soro.next_sibling.text)
 
-		ethnicity_dict["international"] = p_internat
-		ethnicity_dict["uni name"] = uni_name
+				ethnicity = soup.find('th', text="Ethnicity of Students from U.S.")
 
-		if len(ethnicity_dict) != 10:
-			print(uni_name + " " + str(len(ethnicity_dict)))
+				for x in ethnicity.next_sibling.children:
+					if x.string != None:
+						try:
+							perc, nationality = x.string.split("%")
+							row[nationality.strip()] = perc
+						except ValueError: 
+							continue
 
-		# Write the dictionaries to two seperate csv files 
-		#print(ethnicity_dict)
+				internat = soup.find('th', text="International Students").next_sibling.text.split()
+				p_internat = internat[0]
 
-		#t = (uni_name, uni_state.strip(), uni_city, uni_size, enrolled_men, enrolled_women)
+				if p_internat == 'Not':
+					continue
 
-		#print(t)
+				row["International"] = re.sub("%", "", p_internat)
+
+				for field in fieldnames: 
+					if field not in row:
+						continue
+						#row[field] = None
+
+				writer.writerow(row)
+
+if __name__ == '__main__':
+	csv_file = 'ga_college_data.csv'
+	csv_dir = 'CSV'
+
+	html_dir = 'HTML'
+	state = 'GA'
+
+	write_to_csv(os.path.join(csv_dir, csv_file), html_dir, state)
